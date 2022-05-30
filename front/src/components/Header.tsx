ceea338da7e676f844ee8fecb009b9177ee1b7c0
Kelly Douglass <kdouglass@gmail.com>
2022-05-30 11:37:54 -0500

    basic header setup

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import HamburgerMenu from "./HamburgerMenu";
+
+const LeftNav = ({ isMobile }: { isMobile: boolean }) => {
+  if (isMobile) {
+    return (
+      <div className="flex flex-col justify-center">
+        <HamburgerMenu />
+      </div>
+    );
+  }
+  return (
+    <div className="flex justify-around md:w-64 lg:w-96">
+      <h5 className="text-gray-500">Shop</h5>
+      <h5 className="text-gray-500">Categories</h5>
+      <h5 className="text-gray-500">Pages</h5>
+      <h5 className="text-gray-500">Elements</h5>
+    </div>
+  );
+};
+
+const CenterNav = () => {
+  return <h1 className="text-gray-800 text-2xl tracking-widest md:w-60 lg:w-96">SAVOY</h1>;
+};
+
+const RightNav = ({ isMobile }: { isMobile: boolean }) => {
+  if (isMobile) {
+    return <h5>(0)</h5>;
+  }
+  return (
+    <div className="flex justify-between md:w-36">
+      <div className="text-gray-500">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+          />
+        </svg>
+      </div>
+      <h5 className="text-gray-500">Sign in</h5>
+      <h5 className="text-gray-500">Cart</h5>
+    </div>
+  );
+};
 
 export default function Header() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth < 768)
+      setIsMobile(true)
+  }, [])
+
   return (
-    <div>Header</div>
-  )
+    <div className="flex justify-between py-12 px-12">
+      <LeftNav isMobile={isMobile} />
+      <CenterNav />
+      <RightNav isMobile={isMobile} />
+    </div>
+  );
 }
--- front/src/screens/HomeScreen.tsx
+++ front/src/screens/HomeScreen.tsx
@@ -1,10 +1,15 @@
-
-
-import React from 'react'
-import { MainLayout } from '../components/MainLayout'
+import React from "react";
+import { MainLayout } from "../components/MainLayout";
 
 export default function HomeScreen() {
   return (
-    <MainLayout><div>HomeScreen</div></MainLayout>
-  )
+    <MainLayout>
+      <div>
+      <h3 className="text-red-500">HomeScreen</h3>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+    </MainLayout>
+  );
 }
