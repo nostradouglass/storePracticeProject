8eede881362e5769f9d1380456cc9a6894e6b557
Kelly Douglass <kdouglass@gmail.com>
2022-06-29 12:22:32 -0500

    register user front end

--- front/src/components/Register.tsx
+++ front/src/components/Register.tsx
@@ -1,29 +1,61 @@
 import { ColorTypes } from "../Types";
 import { Button } from "./ui/Button";
-import { registerSignInVar } from "../graphql/state"
+import { registerSignInVar } from "../graphql/state";
 import { RegisterSignInType } from "../Types";
+import {ADD_USER} from '../graphql/userMutations'
+import{ useMutation} from '@apollo/client'
 
 export const Register = () => {
+
+  // const [ addUser ] = useMutation(ADD_USER, {
+
+  // })
+
   return (
     <form onSubmit={() => null}>
       <div className="flex flex-col w-full">
         <div className="flex flex-col">
-          <label htmlFor="username" className="text-gray-500 text-left py-2">Email address</label>
-          <input className="border border-1 border-gray-300 p-2" type="text" id="username" value="temp" onChange={() => null} />
+          <label htmlFor="username" className="text-gray-500 text-left py-2">
+            Email address
+          </label>
+          <input
+            className="border border-1 border-gray-300 p-2"
+            type="text"
+            id="username"
+            value="temp"
+            onChange={() => null}
+          />
         </div>
 
         <div className="flex flex-col py-4">
-          <label htmlFor="password" className="text-gray-500 text-left py-2">Password</label>
-          <input className="border border-1 border-gray-300 p-2" type="text" id="username" value="temp" onChange={() => null} />
+          <label htmlFor="password" className="text-gray-500 text-left py-2">
+            Password
+          </label>
+          <input
+          required
+            className="border border-1 border-gray-300 p-2"
+            type="password"
+            id="username"
+            value="temp"
+            onChange={() => null}
+          />
         </div>
 
-    <Button onClick={() => console.log("test")} text="Register" colorType={ColorTypes.DARK} />
-    <div className="flex justify-between w-full items-center">
-      <hr className="flex-grow border-t border-gray-400 mx-4" />
-      <h5 className="text-gray-500">or</h5>
-      <hr className="flex-grow border-t border-gray-400 mx-4" />
-    </div>
-    <Button onClick={() => registerSignInVar(RegisterSignInType.SIGNIN)} text="Sign In" colorType={ColorTypes.LIGHT} />
+        <Button
+          onClick={() => console.log("test")}
+          text="Register"
+          colorType={ColorTypes.DARK}
+        />
+        <div className="flex justify-between w-full items-center">
+          <hr className="flex-grow border-t border-gray-400 mx-4" />
+          <h5 className="text-gray-500">or</h5>
+          <hr className="flex-grow border-t border-gray-400 mx-4" />
+        </div>
+        <Button
+          onClick={() => registerSignInVar(RegisterSignInType.SIGNIN)}
+          text="Sign In"
+          colorType={ColorTypes.LIGHT}
+        />
       </div>
     </form>
   );
