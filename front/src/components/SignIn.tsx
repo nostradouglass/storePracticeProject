5fb3a21fd7af90971b8c9812d4fa7ea2daef85ac
Kelly Douglass <kdouglass@gmail.com>
2022-06-29 12:22:51 -0500

    sign in user front end

--- front/src/components/SignIn.tsx
+++ front/src/components/SignIn.tsx
@@ -1,60 +1,88 @@
 import { ColorTypes } from "../Types";
 import { Button } from "./ui/Button";
-import { registerSignInVar } from "../graphql/state"
+import { registerSignInVar } from "../graphql/state";
 import { RegisterSignInType } from "../Types";
+import { LOGIN_USER } from "../graphql/userQueries";
+import { useLazyQuery } from "@apollo/client";
 import React, { useState } from "react";
 
 export const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberChecked, setRememberedChecked] = useState(false);
 
-  const [userName, setUserName] = useState("")
-  const [password, setPassword] = useState("")
-  const [rememberChecked, setRememberedChecked] = useState(false)
+  const [loginUser, { loading, error, data }] = useLazyQuery(LOGIN_USER);
 
   const submitForm = (e: React.FormEvent) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+
+    loginUser( { variables: { email: email, password: password } }).then((user) => {
+      
+      // if user exists, return the users data. need to set up save user state to graphql state.
+      console.log(user.data.login)
+    }).catch((err) => {
+      console.log(err)
+    })
+    
+  };
 
   return (
     <form onSubmit={submitForm}>
       <div className="flex flex-col w-full">
         <div className="flex flex-col">
-          <label htmlFor="username" className="text-gray-500 text-left py-2">Username or email address</label>
-          <input 
-          className="border border-1 border-gray-300 p-2" 
-          type="text" id="username" 
-          value={userName} 
-          onChange={e => setUserName(e.target.value)} />
+          <label htmlFor="username" className="text-gray-500 text-left py-2">
+            Email address
+          </label>
+          <input
+            className="border border-1 border-gray-300 p-2"
+            type="text"
+            id="username"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
 
         <div className="flex flex-col py-4">
-          <label htmlFor="password" className="text-gray-500 text-left py-2">Password</label>
-          <input 
-          className="border border-1 border-gray-300 p-2" 
-          type="text" id="username" 
-          value={password}
-          onChange={e => setPassword(e.target.value)} />
-        </div>
-      
-        <div className="flex justify-between mt-4">
-            <div className="flex items-center">
-            <input 
-            className="mx-1" 
-            type="checkbox" 
-            checked={rememberChecked}
-            onChange={() => setRememberedChecked(!rememberChecked)}
-            />
-            <h4 className="ml-2">Remember Me</h4>
-            </div>
-            <div>Lost your password?</div>
+          <label htmlFor="password" className="text-gray-500 text-left py-2">
+            Password
+          </label>
+          <input
+            className="border border-1 border-gray-300 p-2"
+            type="password"
+            id="username"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
 
-    <Button onClick={() => console.log("test")} text="Sign In" colorType={ColorTypes.DARK} />
-    <div className="flex justify-between w-full items-center">
-      <hr className="flex-grow border-t border-gray-400 mx-4" />
-      <h5 className="text-gray-500">or</h5>
-      <hr className="flex-grow border-t border-gray-400 mx-4" />
-    </div>
-    <Button onClick={() => registerSignInVar(RegisterSignInType.REGISTER)} text="Create an Account" colorType={ColorTypes.LIGHT} />
+        <div className="flex justify-between mt-4">
+          <div className="flex items-center">
+            <input
+              className="mx-1"
+              type="checkbox"
+              checked={rememberChecked}
+              onChange={() => setRememberedChecked(!rememberChecked)}
+            />
+            <h4 className="ml-2">Remember Me</h4>
+          </div>
+          <div>Lost your password?</div>
+        </div>
+
+        <Button
+          text="Sign In"
+          colorType={ColorTypes.DARK}>
+            <input type="submit" value="Submit" />
+        </Button>
+        <div className="flex justify-between w-full items-center">
+          <hr className="flex-grow border-t border-gray-400 mx-4" />
+          <h5 className="text-gray-500">or</h5>
+          <hr className="flex-grow border-t border-gray-400 mx-4" />
+        </div>
+        <Button
+          onClick={() => registerSignInVar(RegisterSignInType.REGISTER)}
+          text="Create an Account"
+          colorType={ColorTypes.LIGHT}
+        />
       </div>
     </form>
   );
